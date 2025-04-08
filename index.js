b3a488e40ceeeff2bfbb28f22f6280cc0bdd9862
Student <shirleyabwoi@gmail.com>
2025-04-08 17:01:37 +0300

    javascript functions

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
 //1.
-//function arrReverse(){
-    //const days=("Monday","Tuesady","Wednesday")
-    //let reversed=days.sort((a,b)=>(a-b))
-    //return reversed
+function arrReverse(array){
+    let reversed = array.map((item)=> item.split("").sort().join(""))
+    let reversedArr= reversed.sort().reverse()
+    return reversedArr
 
-//}
-//console.log(arrReverse())
+}
+console.log(arrReverse( ['mary', 'shirley', 'bilha']))
 
 //2.
 
-function values(){
-    let valueNumbers= (1,2,3,4,5,6)
-    return valueNumbers.map(num=>{
-        if (num>0)
-            return 'positive'
-        else if (num<0)
-            return 'negative'
-        else
+ function values(array){
+     let valueNumbers= [1,2,-3,4,5,0,6,-7]
+      valueNumbers.forEach(num=>{
+        if (num>0){
+            console.log("positive")
+        }   
+         else if (num<0){
+             console.log("negative")
+            }
+         else{
         return 'zero'
-    })
-}
-//3
+    }
+     })
+ }
 values()
+//3
+ function employees(arrayOfDetails){
+     return arrayOfDetails.sort((a,b)=> a.salary - b.salary)
 
-function employees(){
-    let employeesDetail=("Shirley", "Id", "Salary")
-    return employeesDetail.sort((a,b)=> (a-b))
-}
+ }
+ console.log(employees([{name:"Shirley", id:234, salary: 150000},{name:"Bilha", id:406, salary:50000}]))
 
 //4
-function multiply(){
-    const items=(1,3,5,6,7,9)
-    let d= items.array.forEach(element => {console.log (element*2)
-        
-    });
-}
+ function multiply(array){
+   return array.forEach(element => {console.log (element*2)
+     })};
+ (multiply([1,3,5,6,7,9]));
+ //5
 
-//5
-function calculation(){
-    const arrNumbers=(5,6,3,6,9)
-    let v= arrNumbers.forEach(element=> element.slice(0,4)
-        
-    )
-}
+ function numberOperations(arrayOfNumbers){
+    forEach((item,index) => 
+
+    if(index>4) {item*8}
+    else (numberOperations.length-2 ){item+5})
+    
+
+
+ }
